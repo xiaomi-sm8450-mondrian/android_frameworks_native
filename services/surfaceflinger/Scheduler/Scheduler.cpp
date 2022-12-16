b895cb260483c525972cb9a5eaf8d74e74ef6013
Adithya R <gh0strider.2k18.reborn@gmail.com>
2022-12-17 03:22:53 +0530

    SF: Scheduler: set an expiration for heuristic layer's high refresh rate usages
    Original change by Juhyung Park <qkrwngud825@gmail.com>,
    refactored and adapted to Android 13.
    
    Non-interactive heuristic layer's high refresh rate usages are overkill
    and unnecessary.
    
    Set a hard expiration limit to 3 seconds, or the idle timeout, whichever
    is higher.
    
    This effectively fixes/workarounds a major shortcoming in the whole
    scheduler where it's totally unaware of the view's sizes.
    
    For example, Instagram Reels with audios are played with a full-screen
    video layer and a small animating audio icon at the bottom.
    
    Because the audio icon's animations are lightweight, it's done at the
    full, highest possible refresh rate. This prevents the device from
    entering lower refresh rate states regardless of the video's fps.
    
    With this commit, such layers are not allowed to use > 60 Hz states for
    longer than 3 seconds.
    
    [gotenksIN - Adapt to 14]
    
    Change-Id: I065250bbbe3f0b9ff071779bcc43a4499463ef24
    Co-authored-by: Juhyung Park <qkrwngud825@gmail.com>
    Signed-off-by: Adithya R <gh0strider.2k18.reborn@gmail.com>
    Signed-off-by: Omkar Chandorkar <gotenksIN@aospa.co>

--- services/surfaceflinger/Scheduler/Scheduler.cpp
+++ services/surfaceflinger/Scheduler/Scheduler.cpp
@@ -80,6 +80,7 @@ Scheduler::~Scheduler() {
     // Stop timers and wait for their threads to exit.
     mDisplayPowerTimer.reset();
     mTouchTimer.reset();
+    mHeuristicIdleTimer.reset();
 
     // Stop idle timer and clear callbacks, as the RefreshRateSelector may outlive the Scheduler.
     demotePacesetterDisplay({.toggleIdleTimer = true});
@@ -731,6 +732,9 @@ void Scheduler::onTouchHint() {
         mTouchTimer->reset();
         pacesetterSelectorPtr()->resetKernelIdleTimer();
     }
+    if (mHeuristicIdleTimer) {
+        mHeuristicIdleTimer->reset();
+    }
 }
 
 void Scheduler::setDisplayPowerMode(PhysicalDisplayId id, hal::PowerMode powerMode) {
@@ -819,6 +823,11 @@ void Scheduler::kernelIdleTimerCallback(TimerState state) {
 void Scheduler::idleTimerCallback(TimerState state) {
     applyPolicy(&Policy::idleTimer, state);
     ATRACE_INT("ExpiredIdleTimer", static_cast<int>(state));
+}
+
+void Scheduler::heuristicIdleTimerCallback(TimerState state) {
+    applyPolicy(&Policy::heuristicIdleTimer, state);
+    ALOGV("%s: TimerState %d", __func__, static_cast<int>(state));
 }
 
 void Scheduler::touchTimerCallback(TimerState state) {
@@ -957,6 +966,13 @@ std::shared_ptr<VsyncSchedule> Scheduler::promotePacesetterDisplayLocked(
 
             pacesetter.selectorPtr->startIdleTimer();
         }
+
+        mHeuristicIdleTimer.emplace(
+                "heuristicIdleTimer",
+                std::max(HEURISTIC_TIMEOUT, pacesetter.selectorPtr->getIdleTimerTimeout()),
+                [this] { heuristicIdleTimerCallback(TimerState::Reset); },
+                [this] { heuristicIdleTimerCallback(TimerState::Expired); });
+        mHeuristicIdleTimer->start();
 
         newVsyncSchedulePtr = pacesetter.schedulePtr;
 
@@ -1194,7 +1210,8 @@ GlobalSignals Scheduler::makeGlobalSignals() const {
 
     return {.touch = mTouchTimer && mPolicy.touch == TouchState::Active,
             .idle = mPolicy.idleTimer == TimerState::Expired,
-            .powerOnImminent = powerOnImminent};
+            .powerOnImminent = powerOnImminent,
+            .heuristicIdle = mPolicy.heuristicIdleTimer == TimerState::Expired};
 }
 
 FrameRateMode Scheduler::getPreferredDisplayMode() {
