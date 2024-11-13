c43a683d352661195b4580cf6a93e090a03ecde0
minaripenguin <minaripenguin@users.noreply.github.com>
2024-11-13 16:40:58 +0800

    Optimize dalvik heap config for performance
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>

--- build/phone-xhdpi-6144-dalvik-heap.mk
+++ build/phone-xhdpi-6144-dalvik-heap.mk
@@ -17,9 +17,9 @@
 # Provides overrides to configure the Dalvik heap for a 6GB phone
 
 PRODUCT_VENDOR_PROPERTIES += \
-    dalvik.vm.heapstartsize?=16m \
-    dalvik.vm.heapgrowthlimit?=256m \
-    dalvik.vm.heapsize?=512m \
-    dalvik.vm.heaptargetutilization?=0.5 \
-    dalvik.vm.heapminfree?=8m \
+    dalvik.vm.heapstartsize?=32m \
+    dalvik.vm.heapgrowthlimit?=512m \
+    dalvik.vm.heapsize?=768m \
+    dalvik.vm.heaptargetutilization?=0.75 \
+    dalvik.vm.heapminfree?=4m \
     dalvik.vm.heapmaxfree?=32m
